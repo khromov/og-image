6847fbff0c8729075f3b9c55ee60dd9bbe270a42
Stanislav Khromov <stanislav.khromov+github@gmail.com>
2021-03-28 17:21:23 +0200

    Use radial background

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -21,6 +21,7 @@ function getCss(theme: string, fontSize: string) {
         foreground = 'white';
         radial = 'dimgray';
     }
+
     return `
     @font-face {
         font-family: 'Inter';
@@ -44,9 +45,31 @@ function getCss(theme: string, fontSize: string) {
       }
 
     body {
+        /*
         background: ${background};
         background-image: radial-gradient(circle at 25px 25px, ${radial} 2%, transparent 0%), radial-gradient(circle at 75px 75px, ${radial} 2%, transparent 0%);
         background-size: 100px 100px;
+        
+        background: rgb(246,246,246);
+        background: linear-gradient(90deg, rgba(246,246,246,1) 0%, rgba(208,208,208,1) 100%);
+        */
+
+        /* https://www.magicpattern.design/tools/css-backgrounds */
+
+        /* hex */
+        /*
+        background-color: #ffffff;
+        opacity: 1;
+        background-image:  linear-gradient(30deg, #f9f9f9 12%, transparent 12.5%, transparent 87%, #f9f9f9 87.5%, #f9f9f9), linear-gradient(150deg, #f9f9f9 12%, transparent 12.5%, transparent 87%, #f9f9f9 87.5%, #f9f9f9), linear-gradient(30deg, #f9f9f9 12%, transparent 12.5%, transparent 87%, #f9f9f9 87.5%, #f9f9f9), linear-gradient(150deg, #f9f9f9 12%, transparent 12.5%, transparent 87%, #f9f9f9 87.5%, #f9f9f9), linear-gradient(60deg, #f9f9f977 25%, transparent 25.5%, transparent 75%, #f9f9f977 75%, #f9f9f977), linear-gradient(60deg, #f9f9f977 25%, transparent 25.5%, transparent 75%, #f9f9f977 75%, #f9f9f977);
+        background-size: 60px 105px;
+        background-position: 0 0, 0 0, 30px 53px, 30px 53px, 0 0, 30px 53px;
+        */
+
+        /* radial */
+        background-color: #ffffff;
+        opacity: 1;
+        background-image:  repeating-radial-gradient( circle at 0 0, transparent 0, #ffffff 30px ), repeating-linear-gradient( #f9f9f955, #f9f9f9 );
+
         height: 100vh;
         display: flex;
         text-align: center;
